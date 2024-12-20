58b65b402fbbf62fab80a25f263ba5deb38260ac
Stark <123723458+St4rkXc@users.noreply.github.com>
2024-12-21 00:02:47 +0800

    skills section

--- src/components/landingAbout.vue
+++ src/components/landingAbout.vue
@@ -27,33 +27,33 @@ defineProps({
             About <br />
             <span class="text-secondary">Dhiyo Wikantara</span>
         </h2>
-        <div class="flex mt-16">
+        <div class="flex flex-col lg:flex-row mt-16">
             <!-- Bind active classes dynamically -->
             <p
                 :class="['flex-1 py-6 text-lg font-semibold text-center border-b-2 cursor-pointer text-text', activeTab === 'content1' ? 'border-b-primary' : 'border-b-primary/30']"
                 @click="tabs('content1')"
             >
-                Atrem Project
+                Profile
             </p>
             <p
                 :class="['flex-1 py-6 text-lg font-semibold text-center border-b-2 cursor-pointer text-text', activeTab === 'content2' ? 'border-b-primary' : 'border-b-primary/30']"
                 @click="tabs('content2')"
             >
-                Sejarah Atrem Project
+                Backstory
             </p>
             <p
                 :class="['flex-1 py-6 text-lg font-semibold text-center border-b-2 cursor-pointer text-text', activeTab === 'content3' ? 'border-b-primary' : 'border-b-primary/30']"
                 @click="tabs('content3')"
             >
-                System Internship
+                School
             </p>
         </div>
         <div class="mt-16">
             <!-- Conditional rendering for each tab -->
-            <div v-show="activeTab === 'content1'" class="flex gap-8">
+            <div v-show="activeTab === 'content1'" class="flex flex-col xl:flex-row gap-8">
                 <img :src="atrem" alt="Atrem" />
                 <div>
-                    <h3 class="text-2xl font-bold text-text">Atrem Project</h3>
+                    <h3 class="text-2xl font-bold text-text">Profile</h3>
                     <p class="text-base font-medium text-text leading-[26px]">
                         Atrem Project merupakan hasil dari inisiatif Merta Yoga, yang memulai pendirian agensi ini setelah lulus dari Sekolah Menengah Kejuruan (SMK). Agensi ini berfokus pada
                         penyediaan solusi untuk mendigitalisasi berbagai usaha. Atrem menawarkan berbagai layanan, termasuk pembuatan sistem manajemen laundry, Learning Management System (LMS), jasa
@@ -61,10 +61,10 @@ defineProps({
                     </p>
                 </div>
             </div>
-            <div v-show="activeTab === 'content2'" class="flex gap-8">
+            <div v-show="activeTab === 'content2'" class="flex flex-col xl:flex-row gap-8">
                 <img :src="historyImg" alt="History" class="grayscale" />
                 <div>
-                    <h3 class="text-2xl font-bold text-text">Sejarah Atrem Project</h3>
+                    <h3 class="text-2xl font-bold text-text">Backstory</h3>
                     <p class="text-base font-medium text-text leading-[26px]">
                         Semua berawal dari ketertarikan Merta Yoga pada bidang komputer, terutama setelah ia mengetahui bahwa seorang programmer dapat bekerja secara remote. Hal ini mendorongnya untuk
                         memilih SMK 1 Denpasar dengan jurusan Rekayasa Perangkat Lunak (RPL) sebagai langkah awal dalam kariernya. Setelah menyelesaikan pendidikan di SMK, Merta Yoga memutuskan untuk
@@ -72,10 +72,10 @@ defineProps({
                     </p>
                 </div>
             </div>
-            <div v-show="activeTab === 'content3'" class="flex gap-8">
+            <div v-show="activeTab === 'content3'" class="flex flex-col xl:flex-row gap-8">
                 <img :src="internImg" alt="Internship" class="grayscale" />
                 <div>
-                    <h3 class="text-2xl font-bold text-text">System Internship</h3>
+                    <h3 class="text-2xl font-bold text-text">School</h3>
                     <p class="text-base font-medium text-text leading-[26px]">
                         Atrem Project merekrut anggota agensi melalui sistem magang, di mana setiap angkatan disebut sebagai "batch." Saya termasuk dalam batch 6. Setiap anggota batch akan diberikan
                         tugas sesuai dengan bidang yang mereka pilih. Jika salah satu anggota batch dinilai kompeten oleh Merta Yoga, mereka akan ditawari kesempatan untuk bergabung dengan Atrem
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,9 +2,11 @@
 import landingNavbar from "@/components/landingNavbar.vue";
 import landingHero from "@/components/landingHero.vue";
 import landingAbout from "@/components/landingAbout.vue";
+import landingSkills from "@/components/landingSkills.vue";
 </script>
 <template>
     <landingNavbar />
 	<landingHero title="Dhiyo (Stark) Wikantara" subtitle="UI/UX Engineer & Frontend Developer " />
 	<landingAbout />
+	<landingSkills />
 </template>
