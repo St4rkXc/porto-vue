5dc33b5598ee040f7dc224962ef6af395169eee0
Stark <123723458+St4rkXc@users.noreply.github.com>
2024-12-25 13:20:41 +0800

    aos on all section in landing

--- src/components/landingProjects.vue
+++ src/components/landingProjects.vue
@@ -8,46 +8,58 @@ import packageProject from '@/assets/images/package-thumn.png';
 </script>
 <template>
     <section class="py-[100px] my-container" id="projects">
-        <p class="text-center section-title"
+        <p class="text-center section-title" data-aos="fade" data-aos-duration="1500" 
             >My Projects & <br />
             <span class="text-secondary">Works</span></p
         >
         <div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 mt-16 gap-4">
-            <projectsCompo 
-                :projectImg=protoProject
-                projectTitle="16 UI Prototyping Project"
-                :badges="['Figma', 'Illustrator']"
-                links="/Projects/ProtoProject"
-            />
-            <projectsCompo 
-                :projectImg=uiProject
-                projectTitle="28 UI Projects"
-                :badges="['Figma', 'illustrator']"
-                links="/Projects/UiProject"
-            />
-            <projectsCompo 
-                :projectImg=miraProject
-                projectTitle="Mira Project Study Case"
-                :badges="['Figma', 'Miro']"
-                links="/Projects/MiraProject"
-            />
-            <projectsCompo 
-                :projectImg=lmsProject
-                projectTitle="LMS Project (Frontend)"
-                :badges="['Tailwind', 'Blade', 'DaisyUI']"
-                links="/Projects/LmsProject"
-            />
-            <projectsCompo 
-                :projectImg=packageProject
-                projectTitle="Package Tracking System"
-                :badges="['Tailwind', 'Blade', 'DaisyUI']"
-                links="/Projects/PackageProject"
-            />
-            <projectsCompo 
-                :projectImg=protoProject
-                projectTitle="Laundry Management System"
-                :badges="['Figma', 'Miro']"
-            />
+            <div data-aos="fade-up" data-aos-duration="1500">
+                <projectsCompo
+                    :projectImg=protoProject
+                    projectTitle="16 UI Prototyping Project"
+                    :badges="['Figma', 'Illustrator']"
+                    links="/Projects/ProtoProject"
+                />
+            </div>
+            <div data-aos="fade-up" data-aos-duration="1500" data-aos-delay="300">
+                <projectsCompo
+                    :projectImg=uiProject
+                    projectTitle="28 UI Projects"
+                    :badges="['Figma', 'illustrator']"
+                    links="/Projects/UiProject"
+                />
+            </div>
+            <div data-aos="fade-up" data-aos-duration="1500" data-aos-delay="600">
+                <projectsCompo
+                    :projectImg=miraProject
+                    projectTitle="Mira Project Study Case"
+                    :badges="['Figma', 'Miro']"
+                    links="/Projects/MiraProject"
+                />
+            </div>
+            <div data-aos="fade-up" data-aos-duration="1500" data-aos-delay="900">
+                <projectsCompo
+                    :projectImg=lmsProject
+                    projectTitle="LMS Project (Frontend)"
+                    :badges="['Tailwind', 'Blade', 'DaisyUI']"
+                    links="/Projects/LmsProject"
+                />
+            </div>
+            <div data-aos="fade-up" data-aos-duration="1500" data-aos-delay="1200">
+                <projectsCompo
+                    :projectImg=packageProject
+                    projectTitle="Package Tracking System"
+                    :badges="['Tailwind', 'Blade', 'DaisyUI']"
+                    links="/Projects/PackageProject"
+                />
+            </div>
+            <div data-aos="fade-up" data-aos-duration="1500" data-aos-delay="1500">
+                <projectsCompo
+                    :projectImg=protoProject
+                    projectTitle="Laundry Management System"
+                    :badges="['Figma', 'Miro']"
+                />
+            </div>
         </div>
     </section>
 </template>
