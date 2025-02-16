45a605ead258a7c8b8aed30809b6f3b12014c927
Stark <123723458+St4rkXc@users.noreply.github.com>
2025-02-16 20:12:26 +0800

    landing about 2 testting

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,18 +2,19 @@
 import landingNavbar from "@/components/Navbar/landingNavbar.vue";
 import landingHero from "@/components/Landing/landingHero.vue";
 import landingAbout from "@/components/Landing/landingAbout.vue";
+import LandingAbout2 from "@/components/Landing/LandingAbout2.vue";
 import landingSkills from "@/components/Landing/landingSkills.vue";
 import LandingExperience from "@/components/Landing/landingExperience.vue";
 import landingProjects from "@/components/Landing/landingProjects.vue";
 import landingFooter from "@/components/Landing/landingFooter.vue";
-
+import LandingAbout from "@/components/Landing/landingAbout.vue";
 </script>
 <template>
     <landingNavbar />
-	<landingHero title="Dhiyo (Stark) Wikantara" subtitle="UI/UX Engineer & Frontend Developer " />
-	<landingAbout />
-	<landingSkills />
-	<LandingExperience />
-	<landingProjects />
-	<landingFooter />
+    <landingHero title="Dhiyo (Stark) Wikantara" subtitle="UI/UX Engineer & Frontend Developer " />
+    <LandingAbout/>
+    <landingSkills />
+    <LandingExperience />
+    <landingProjects />
+    <landingFooter />
 </template>
